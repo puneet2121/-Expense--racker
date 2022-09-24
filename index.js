07807d04572a6edbb420cf18c069b57491297e1b
Aarav <aaravuno8@gmail.com>
2022-09-24 11:54:54 -0600

    Added firebase config

--- index.js
+++ index.js
@@ -2,7 +2,33 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ4mG0vJeljtXc7XOPSHw2egBI-Jmy5_U",
+  authDomain: "expense-tracker-9f3eb.firebaseapp.com",
+  projectId: "expense-tracker-9f3eb",
+  storageBucket: "expense-tracker-9f3eb.appspot.com",
+  messagingSenderId: "783379826189",
+  appId: "1:783379826189:web:b8f0336adeee6533d0f744",
+  measurementId: "G-RRKN85FY07"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/users', (req, res) => {
   res.send('Hello World!')
 })
 
