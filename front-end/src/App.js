99f9406452ddef2db3a29d5bb5b3927240f44227
puneet2121 <bedipuneet21@gmail.com>
2022-09-24 23:35:34 -0600

    add local stroage to the login component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,20 @@
 import React, { useState } from "react";
 import Login from "./components/Login";
 import Home from "./components/Home";
-import { Link, Route, Routes,BrowserRouter } from "react-router-dom";
+import { Link, Route, Routes, BrowserRouter } from "react-router-dom";
 function App() {
-  const [isLogin, setIslogin] = useState(false);
+  const [isLogin, setIslogin] = useState('');
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />}></Route>
-      
-        <Route path="/home" element={<Home />} ></Route>
-      
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Login login={isLogin} setIslogin={setIslogin} />}></Route>
+
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-{
-  /* <div className="App">
-      {isLogin ? <Home /> : <Login />}
 
-    </div> */
-}
--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -2,34 +2,42 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Home from "./Home";
-import { Link, Route, Routes,Redirect } from "react-router-dom";
+import {  useNavigate,Link } from "react-router-dom";
+import { useState } from "react";
 const Login = (props) => {
-  const handleClick = (e) => {
+  const navigate = useNavigate();
+  const [name,setName] = useState('');
+  const [password,setPassword] = useState('');
+  const [isauth,setIsauth] = useState(localStorage.getItem(localStorage.getItem('isauth') || false));
+  const users = [ {name: 'Puneet', password:'12345'} ];
+  const handleSubmit = (e) => {
     e.preventDefault();
-    
-      
+    const account = users.find((user) => user.name === name);
+    if(account && account.password === password) {
+      localStorage.setItem('isauth',true);
+      navigate('/home');
+    }
   };
   return (
     <>
       <div className="container ">
         <div className="d-flex">
-          <Form className="mt-3 w-50 justify-content-center">
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+          <Form
+            className="mt-3 w-50 justify-content-center" onSubmit = {handleSubmit}
+            
+          >
+            <Form.Group className="mb-3" controlId="formBasicEmail" >
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter Name" value={name} onChange={(e) => setName(e.target.value)} />
+              
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" onClick={handleClick} type="submit">
+            
+            <Button variant="primary" type="submit" >
               Submit
             </Button>
           </Form>
