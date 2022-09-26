2452bfbeb5c4e3ba5b6e645e26da0dbe438f64df
puneet2121 <bedipuneet21@gmail.com>
2022-09-25 18:36:17 -0600

    add signup component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import Signup from "./components/Signup";
 import { Link, Route, Routes, BrowserRouter } from "react-router-dom";
 function App() {
   const [isLogin, setIslogin] = useState('');
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login login={isLogin} setIslogin={setIslogin} />}></Route>
 
         <Route path="/home" element={<Home />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
       </Routes>
     </BrowserRouter>
   );
