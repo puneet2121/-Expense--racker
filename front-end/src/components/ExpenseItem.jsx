db94a6c1ca3fa78d29ae27541113bcde9fed97c4
puneet2121 <bedipuneet21@gmail.com>
2022-10-10 15:27:17 -0600

    adding day method

--- front-end/src/components/ExpenseItem.jsx
+++ front-end/src/components/ExpenseItem.jsx
@@ -6,7 +6,11 @@ const ExpenseItem = () => {
   const expenseAmount = 450;
   return (
     <div className="expense-item">
-      <div>gg</div>
+      <div>
+        <div>{expenseDate.toLocaleDateString('en-us', { month: 'long'})}</div>
+        <div>{expenseDate.toLocaleDateString('en-us', { year: 'numeric'})}</div>
+        <div>{expenseDate.toLocaleDateString('en-us', { day: 'numeric'})}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">{expenseAmount}</div>
