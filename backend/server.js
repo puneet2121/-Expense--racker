1ca8b6189185eb8ceddd22289d6c79924bbe9ec0
puneet2121 <bedipuneet21@gmail.com>
2022-12-11 13:48:09 -0700

    add backend

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
-import express, { application } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectToDB from './config.js';
-
+dotenv.config();
 const port = process.env.PORT
 // middleware
-dotenv.config();
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 
 connectToDB();
+
+app.get('/',(req,res) => {
+  res.send("hello world");
+}
+)
 
 app.listen(port,() => console.log(`server is listening on ${port}`));
---
+++ front-end/src/components/ExpensesList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../style/ExpensesList.css'
+const ExpensesList = () => {
+  return (
+    <div>ExpensesList</div>
+  )
+}
+
+export default ExpensesList
